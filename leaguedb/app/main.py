ff54135421cf47b92f524573d26968e954d73dd2
Ben Yang <ben.yang@utexas.edu>
2017-06-17 10:37:30 -0500

    Configured bootstrap and added navbar.

--- leaguedb/app/main.py
+++ leaguedb/app/main.py
@@ -1,22 +1,12 @@
-from pyfiglet import Figlet
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-font = Figlet(font="starwars")
-
 @app.route("/")
-def hello():
-    message = os.getenv("MESSAGE", "WELCOME\nTO THE LEAGUE OF\nDRAVEN")
+@app.route("/<page>")
+def home(page=None):
+    return render_template("home.html", page=page)
 
-    html_text = font.renderText(message)\
-                    .replace(" ", "&nbsp;")\
-                    .replace(">", "&gt;")\
-                    .replace("<", "&lt;")\
-                    .replace("\n", "<br>")
-
-    return "<html><body style='font-family: mono;'>" + html_text + "</body></html>"
-    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
