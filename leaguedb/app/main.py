8250011d04d07761337ffa0d4ff490b27fbd26f3
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-06-17 22:06:23 -0500

    types galore

--- leaguedb/app/main.py
+++ leaguedb/app/main.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/<page>")
-def home(page=None):
+def home(page: str = None):
     return render_template("home.html", page=page)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
