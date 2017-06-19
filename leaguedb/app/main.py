e56f9340ad4d983e4cf5a0dd03f716e7948905e8
Tschriber <rts@cs.utexas.edu>
2017-06-19 01:24:57 -0500

    added basic champ pages

--- leaguedb/app/main.py
+++ leaguedb/app/main.py
@@ -1,13 +1,19 @@
 import os
 from flask import Flask, render_template
 
-app: Flask = Flask(__name__)
-
+app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<page>")
-def home(page: str = None):
+def home(page=None):
     return render_template("home.html", page=page)
+def champions(page=None):
+	return render_template("champions.html", page=page)
+def Jax(page=None):
+	return render_template("Jax.html", page=page)
+def Khazix(page=None):
+	return render_template("Kha'Zix.html", page=page)
+def Caitlyn(page=None):
+	return render_template("Caitlyn.html", page=page)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
