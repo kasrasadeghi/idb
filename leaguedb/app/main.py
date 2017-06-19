b96492d29dff2109f2c87d1c2e45849ad2ec63d9
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-06-19 18:31:24 -0500

    preliminary about page and routing

--- leaguedb/app/main.py
+++ leaguedb/app/main.py
@@ -1,18 +1,35 @@
-import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/<page>")
-def home(page=None):
+def home(page: str = None) -> str:
     if page:
-        return render_template(page+".html", page=page)
+        return render_template(page+".html")
     return render_template("home.html", page=page)
 
+
 @app.route("/champions/<name>")
-def champions(name=None):
+def champion_route(name: str) -> str:
     return render_template(name + ".html", name=name)
+
+
+@app.route("/items/<name>")
+def item_route(name: str) -> str:
+    return render_template("item.html", name=name)
+
+
+@app.route("/class/<name>")
+def class_route(name: str) -> str:
+    return render_template("class.html", name=name)
+
+
+@app.route("/roles/<name>")
+def role_route(name: str) -> str:
+    return render_template("role.html", name=name)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
