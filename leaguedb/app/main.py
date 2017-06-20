5f8ae46a3a2859e98635f69eee73da49b21550e6
Ben Yang <ben.yang@utexas.edu>
2017-06-19 21:48:00 -0500

    NO TYPES

--- leaguedb/app/main.py
+++ leaguedb/app/main.py
@@ -6,29 +6,29 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<page>")
-def home(page: str = None) -> str:
+def home(page = None):
     if page:
         return render_template(page+".html")
     return render_template("home.html", page=page)
 
 
 @app.route("/champions/<name>")
-def champion_route(name: str) -> str:
+def champion_route(name):
     return render_template("champions/" + name + ".html", name=name)
 
 
 @app.route("/items/<name>")
-def item_route(name: str) -> str:
+def item_route(name):
     return render_template("items/" + name + ".html", name=name)
 
 
 @app.route("/classes/<name>")
-def class_route(name: str) -> str:
+def class_route(name):
     return render_template("classes/" + name + ".html", name=name)
 
 
 @app.route("/roles/<name>")
-def role_route(name: str) -> str:
+def role_route(name):
     return render_template("roles/" + name + ".html", name=name)
 
 
