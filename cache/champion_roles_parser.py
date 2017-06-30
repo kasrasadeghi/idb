30ae904a0de970a7700c90a0ee620ab7d95fbdbd
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-06-30 18:14:02 -0500

    hotfix

--- cache/champion_roles_parser.py
+++ cache/champion_roles_parser.py
@@ -5,12 +5,17 @@ from typing import Dict, List
 def parse(filename: str) -> Dict[str, List[str]]:
     file = open(filename, "r")
     result = {}
+    file_iter = iter(file)
+    next(file_iter)
+    next(file_iter)
     for line in file:
-        split = line.split(":")
+        split = list(map(lambda x: x.strip(), line.split(":")))
         name = split[0]
-        roles = split[1].split(",")
+        roles = list(map(lambda x: x.strip(), split[1].split(",")))
         result[name] = roles
     return result
 
 if __name__ == "__main__":
-    parse("champion-roles.txt")
+    d = parse("champion-roles.txt")
+    for name, roles in d.items():
+        print(name, roles)
