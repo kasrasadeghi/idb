871dd589fb99580711b8761f41bed6ec3e81e391
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-07-01 19:50:29 -0500

    documentation and specification

--- leaguedb/app/main.py
+++ leaguedb/app/main.py
@@ -58,72 +58,213 @@ def role_route(name: str) -> str:
 #####
 
 
+#
+# region champions
+#
+
+
 @app.route("/api/champion_names")
 def api_get_champion_names() -> str:
+    """
+    gets a list of champion names in a json
+
+    :return: a list of champion names
+
+    { "Thresh", "Viktor", "Hecarim", ... }
+    """
     f = open("static/champion_names.json", "r")
     contents = json.load(f)
-    print(type(contents))
     json_resp = contents
-    resp = jsonify(json_resp)
-    print(json_resp)
+    resp = jsonify(json_resp)  # this is a response object
     return resp
-
-#
-# champions
-#
 
 
 @app.route("/api/champions")
 def api_champions() -> str:
+    """
+    gets a json with all champion information
+
+    :return: the response
+
+    [
+        {
+            "name": "Quinn",
+            "roles": [ ... ],
+            "classes": [ ... ],
+            "items": [ ... ],
+            "lore": "Quinn and ...
+            "icon": "Quinn.png"
+        },
+        ...
+    ]
+    """
     pass
 
 
 @app.route("/api/champion/<name>")
 def api_champion(name: str) -> str:
+    """
+    gets a single champion by name
+
+    :param name: the name of the champion
+    :return: the response
+
+    {
+        "name": "Quinn",
+        "roles": [ ... ],
+        "classes": [ ... ],
+        "items": [ ... ],
+        "lore": "Quinn and ...
+        "icon": "Quinn.png"
+    }
+    """
     pass
 
+# endregion
 #
-# classes
+
+
+#
+# region classes
 #
 
 
 @app.route("/api/classes")
 def api_classes() -> str:
+    """
+    gets all class in a json
+
+    :return: the response
+
+    [
+        {
+            "name": "Marksman",
+            "icon": "Marksman.png",
+            "description": "Marksman are ranged ...
+            "champions": [ ...],
+            "items": [ ... ]
+        },
+        ...
+    ]
+    """
     pass
 
 
 @app.route("/api/class/<name>")
 def api_class(name: str) -> str:
+    """
+    gets a single class's json
+
+    :param name: the name of a specific class
+    :return: the response
+
+    {
+        "name": "Marksman",
+        "icon": "Marksman.png",
+        "description": "Marksman are ranged ...
+        "champions": [ ...],
+        "items": [ ... ]
+    }
+    """
     pass
+
+# endregion
+#
 
 
 #
-# items
+# region items
 #
 
 
 @app.route("/api/items")
 def api_items() -> str:
+    """
+    gets all item data in a json
+
+    :return: the response
+    [
+        {
+            "name": "B. F. Sword",
+            "roles": [ ... ],
+            "champions": [ ... ],
+            "categories": [ ... ],
+            "image": "1038.png"
+        },
+        ...
+    ]
+    """
     pass
 
 
 @app.route("/api/item/<name>")
 def api_item(name: str) -> str:
+    """
+    gets a single item's information
+
+    :param name: the specific item's name
+    :return: the response
+
+    {
+        "name": "B. F. Sword",
+        "roles": [ ... ],
+        "champions": [ ... ],
+        "categories": [ ... ],
+        "image": "1038.png"
+    }
+    """
     pass
 
+# endregion
 #
-# roles
+
+
+#
+# region roles
 #
 
 
 @app.route("/api/roles")
 def api_roles() -> str:
+    """
+    gets all of the roles
+
+    :return: the response
+
+    [
+        {
+            "name": "Sup",
+            "icon": "sup.png",
+            "classes": [ ... ],
+            "items": [ ... ],
+            "champions" : [ ... ]
+        },
+        ...
+    ]
+    """
     pass
 
 
 @app.route("/api/role/<name>")
 def api_role(name: str) -> str:
+    """
+    gets a single role by name
+
+    :param name: the role's name
+    :return: the response
+
+    {
+        "name": "Sup",
+        "icon": "sup.png",
+        "classes": [ ... ],
+        "items": [ ... ],
+        "champions" : [ ... ]
+    }
+    """
     pass
+
+# endregion
+#
 
 
 if __name__ == "__main__":
