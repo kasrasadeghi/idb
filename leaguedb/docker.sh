0c766964a8deff1c670ffafdb349e51fdd7df310
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-07-03 22:52:22 +0000

    fixed docker.sh

--- leaguedb/docker.sh
+++ leaguedb/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker rm leaguedb_run
 docker kill leaguedb_run
+docker rm leaguedb_run
 docker build -t leaguedb_app .
+docker rmi $(docker images -a | grep "^<none>" | awk '{print $3}')
 docker run -d --name leaguedb_run --restart=always -p 80:80 -t leaguedb_app
