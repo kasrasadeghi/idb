ddd82dbb7fa1ea965394019d11ac8cbab2ccd8b8
Ben Yang <ben.yang@utexas.edu>
2017-07-15 15:59:43 -0500

    Edit form resets

--- react/leaguedb/src/components/Edit.js
+++ react/leaguedb/src/components/Edit.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, ReactDOM} from 'react';
 import {
   Container,
   Col,
@@ -6,44 +6,110 @@ import {
   Form,
   FormGroup,
   Label,
-  Input,
-  FormText
+  Input
 } from 'reactstrap';
 
 class Edit extends Component {
   constructor() {
     super();
     this.state = {
-      model: "champion"
+      model: "champion",
+      key: ""
     }
 
+    this.changeKey = this.changeKey.bind(this);
     this.changeModel = this.changeModel.bind(this);
+    this.changeAttrs = this.changeAttrs.bind(this);
   }
 
-  changeModel(event) {
+  changeKey(event) {
     this.setState({
-      model: event.target.value
+      key: event.target.value
     });
   }
 
-  printState() {
+  changeModel(event) {
+    var new_model = event.target.value;
+    var old_key = this.state.key;
+    // clear form
+    document.getElementById("form").reset();
+    // Reset state
+    this.state = { model: "", key: "" };
+    this.setState({
+      model: new_model,
+      key: old_key
+    });
+  }
+
+  changeAttrs(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  submitEdit() {
     console.log(this.state);
+  }
+
+  properCaps(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  textEntry(name) {
+    return ( 
+      <FormGroup row>
+        <Label sm={{size: 2, offset: 2}}>{this.properCaps(name)}</Label>
+        <Col sm={8}>
+          <Input name={name} onChange={this.changeAttrs}/>
+        </Col>
+      </FormGroup>
+    );
+  }
+
+  renderOptions() {
+    var result = [];
+    result.push(this.textEntry("name"));
+    result.push(this.textEntry("icon"));
+    switch (this.state.model) {
+      case "champion":
+        result.push(this.textEntry("classes"));
+        result.push(this.textEntry("items"));
+        result.push(this.textEntry("lore"));
+        result.push(this.textEntry("roles"));
+        break;
+      case "item":
+        result.push(this.textEntry("categories"));
+        result.push(this.textEntry("classes"));
+        result.push(this.textEntry("roles"));
+        break;
+      case "class":
+        result.push(this.textEntry("champions"));
+        result.push(this.textEntry("description"));
+        result.push(this.textEntry("items"));
+        break;
+      case "role":
+        result.push(this.textEntry("champions"));
+        result.push(this.textentry("classes"));
+        result.push(this.textentry("items"));
+        break;
+    }
+    return result;
   }
 
   render() {
     return (
       <Container>
-        <Form>
+        <Form id="form">
           <FormGroup row>
-            <Label for="editPass" sm={2}>Password</Label>
+            <Label sm={2}>Key</Label>
             <Col sm={10}>
-              <Input type="password" />
+              <Input type="password" value={this.state.key} onChange={this.changeKey} />
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label for="modelSelect" sm={2}>Model</Label>
+            <Label sm={2}>Model</Label>
             <Col sm={10}>
-              <Input type="select" name="select" value={this.state.model} onChange={this.changeModel}>
+              <Input type="select" value={this.state.model} onChange={this.changeModel}>
                 <option value="champion"> Champion </option>
                 <option value="item"> Item </option>
                 <option value="class"> Class </option>
@@ -51,15 +117,10 @@ class Edit extends Component {
               </Input>
             </Col>
           </FormGroup>
-          <FormGroup row>
-            <Label for="reasonText" sm={2}>Reason</Label>
-            <Col sm={10}>
-              <Input type="textarea" name="text" id="reasonText" />
-            </Col>
-          </FormGroup>
+          {this.renderOptions()}
           <FormGroup check row>
-            <Col sm={{ size: 10, offset: 2 }}>
-              <Button onClick={() => this.printState()}>Submit</Button>
+            <Col sm={{ size: 2, offset: 10 }}>
+              <Button onClick={() => this.submitEdit()}>Submit</Button>
             </Col>
           </FormGroup>
         </Form>
