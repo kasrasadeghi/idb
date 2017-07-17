8930bed8b8106d2ece3d5daa43eecf31982f1ad7
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-07-17 01:50:14 -0500

    updated about page with my specs

--- react/leaguedb/src/components/About.js
+++ react/leaguedb/src/components/About.js
@@ -13,57 +13,10 @@ import {
   Table
 } from 'reactstrap';
 
-class LeagueBar extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      isOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      isOpen: !(this.state.isOpen)
-    });
-  }
-
-  render() {
-    return (
-      <Navbar color="faded" light toggleable>
-        <NavbarToggler right onClick={() => this.toggle()}/>
-        <NavbarBrand href="/">LeagueDB</NavbarBrand>
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/search">Search</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/champions">Champions</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/items">Items</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/classes">Classes</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/roles">Roles</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/about">About</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    )
-  }
-}
-
 class About extends Component {
   constructor() {
     super();
-    
+
     this.state = {
       commits: {
         "SpicyKitten": 0,
@@ -87,8 +40,8 @@ class About extends Component {
       var updatedCommits = {}
       var sum = 0;
       for (var i = 0; i < j.length; i++) {
-         updatedCommits[j[i].author.login] = j[i].total;
-         sum += j[i].total;
+        updatedCommits[j[i].author.login] = j[i].total;
+        sum += j[i].total;
       }
       updatedCommits['total'] = sum;
       this.setState({
@@ -96,11 +49,11 @@ class About extends Component {
       });
     });
 
-    
+
     var members = ["avilashrath", "toddschriber", "benyangs", "kasrasadeghi1"];
     for (var mem in members) {
       (mem => {
-        fetch('https://api.trello.com/1/members/' + mem  + '/cards', {
+        fetch('https://api.trello.com/1/members/' + mem + '/cards', {
           method: 'GET',
           dataType: 'json'
         })
@@ -138,13 +91,16 @@ class About extends Component {
   render() {
     return (
       <div>
-        <LeagueBar/>
         <Container>
           <br />
           <br />
           <h2>Purpose</h2>
           <p>
-            This site contains information about the game League of Legends. Interested parties can find information about playable champions, roles and classes of each champion, as well as recommended items for each champion or class.
+            This site contains information about the game League of Legends. Interested parties can
+            find information
+            about playable champions, roles and classes of each champion, as well as recommended
+            items for each champion
+            or class.
           </p>
           <br />
           <h2>Our Bronze 5 Staff</h2>
@@ -155,13 +111,19 @@ class About extends Component {
               <img src="static/images/profile/Kasra.jpg"/>
               <br />
               <h3> Kasra </h3>
-              Kasra tell us a bit about yourself
+              I'm a computer science and math major at UT Austin. I like programming languages,
+              compilers, playing guitar, and cooking.
               <br/>
               <br />
               Responsibilities:
+              <ul>
+                <li>Server maintenance and setup</li>
+                <li>Docker integration</li>
+                <li>React implementation</li>
+                <li>Build system design</li>
+              </ul>
               <br/>
               <br />
-              Not killing himself
               <br/>
               <br />
               No. of Commits: {this.state.commits['kasrasadeghi']}
@@ -174,9 +136,9 @@ class About extends Component {
               <br/>
               <br />
             </Col>
-          <Col xs="6">
+            <Col xs="6">
               <br />
-              <img src="static/images/profile/Ben.jpg" />
+              <img src="static/images/profile/Ben.jpg"/>
               <br />
               <h3> Ben </h3>
               I'm a Junior computer science and math major at UT Austin. I like
@@ -258,103 +220,105 @@ class About extends Component {
             <Col>
               <Table bordered>
                 <thead>
-                  <tr>
-                    <th>Metric</th>
-                    <th>Number</th>
-                  </tr>
+                <tr>
+                  <th>Metric</th>
+                  <th>Number</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Commits</td>
-                    <td> { this.state.commits['total'] }
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Issues</td>
-                    <td>{this.state.avii + this.state.todi + this.state.beni + this.state.kasi}</td>
-                  </tr>
-                  <tr>
-                    <td>Unit Tests</td>
-                    <td>19</td>
-                  </tr>
+                <tr>
+                  <td>Commits</td>
+                  <td> { this.state.commits['total'] }
+                  </td>
+                </tr>
+                <tr>
+                  <td>Issues</td>
+                  <td>{this.state.avii + this.state.todi + this.state.beni + this.state.kasi}</td>
+                </tr>
+                <tr>
+                  <td>Unit Tests</td>
+                  <td>19</td>
+                </tr>
                 </tbody>
               </Table>
             </Col>
             <Col>
               <Table bordered>
                 <thead>
-                  <tr>
-                    <th>Sources</th>
-                  </tr>
+                <tr>
+                  <th>Sources</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>
-                      <a href="http://artbook.na.leagueoflegends.com/en_US/volume-one">Art</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <a href="https://developer.riotgames.com/">League of Legends API</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <a href="http://leagueoflegends.wikia.com/api/v1/">LoL Wiki API</a>
-                    </td>
-                  </tr>
+                <tr>
+                  <td>
+                    <a href="http://artbook.na.leagueoflegends.com/en_US/volume-one">Art</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="https://developer.riotgames.com/">League of Legends API</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="http://leagueoflegends.wikia.com/api/v1/">LoL Wiki API</a>
+                  </td>
+                </tr>
                 </tbody>
               </Table>
             </Col>
             <Col>
               <Table bordered>
                 <thead>
-                  <tr>
-                    <th>Tools</th>
-                  </tr>
+                <tr>
+                  <th>Tools</th>
+                </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Flask, SQLAlchemy</td>
-                  </tr>
-                  <tr>
-                    <td>Bootstrap, React</td>
-                  </tr>
-                  <tr>
-                    <td>PostgreSQL</td>
-                  </tr>
+                <tr>
+                  <td>Flask, SQLAlchemy</td>
+                </tr>
+                <tr>
+                  <td>Bootstrap, React</td>
+                </tr>
+                <tr>
+                  <td>PostgreSQL</td>
+                </tr>
                 </tbody>
               </Table>
             </Col>
             <Col>
               <Table bordered>
                 <tbody>
-                  <tr>
-                    <td>
-                      <a href="https://github.com/kasrasadeghi/idb">Github Repo</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <a href="http://docs.leaguedbapi.apiary.io/">League DB API Docs</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <a href="https://trello.com/b/6BIgkiSa/leaguedb-general">Trello Issue Tracker</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <a href="https://yuml.me/78eb6a39">yUML Diagram</a>
-                    </td>
-                  </tr>
+                <tr>
+                  <td>
+                    <a href="https://github.com/kasrasadeghi/idb">Github Repo</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="http://docs.leaguedbapi.apiary.io/">League DB API Docs</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="https://trello.com/b/6BIgkiSa/leaguedb-general">Trello Issue
+                      Tracker</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="https://yuml.me/78eb6a39">yUML Diagram</a>
+                  </td>
+                </tr>
                 </tbody>
               </Table>
             </Col>
           </Row>
           <br />
-          <h2> <a href="https://utexas.box.com/s/9oe95gfnj8n2p13y84tlbea6q1vl7mk8" >Technical Report</a> </h2>
+          <h2><a href="https://utexas.box.com/s/9oe95gfnj8n2p13y84tlbea6q1vl7mk8">Technical
+            Report</a></h2>
           <br />
         </Container>
       </div>
